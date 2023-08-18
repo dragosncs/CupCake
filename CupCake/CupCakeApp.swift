0b3d4cd1b0890d0cd35275526f9595957edf073d
Dragos Neacsu <dragosncs@gmail.com>
2023-08-18 20:14:12 +0300

    Initial Commit

---
+++ CupCake/CupCakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeApp.swift
+//  CupCake
+//
+//  Created by Dragos Neacsu on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
